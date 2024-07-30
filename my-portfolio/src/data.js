71be97fa742f5768f2f883094778b0f57310cbc3
Paras jaggi <83832920+parasjaggi@users.noreply.github.com>
2024-07-30 23:00:44 +0530

    Second Commit

---
+++ my-portfolio/src/data.js
@@ -0,0 +1,28 @@
+export const projects = [
+  {
+    title: "DocuGuard",
+    subtitle: "YOLO MODEl",
+    description:
+      "DocuGuard is a web application designed to detect the use of whitener and instances of overwriting on scanned document images. Using a custom-trained machine learning model, DocuGuard can identify tampered areas and highlight them with bounding boxes. This project is ideal for document verification processes, ensuring the authenticity and integrity of paperwork.",
+    image: "./image1.jpg",
+    link: "https://github.com/parasjaggi/forgery",
+  },
+  {
+    title: "Path-Finding-Algorithm-Visualizer",
+    subtitle: "DSA Visualized",
+    description:
+      "Path-finding algorithms are algorithms used to find optimal path between two locations. These algorithms are widely used in map applications like Google Maps, for example.",
+    image: "./image2.jpg",
+    link: "https://github.com/parasjaggi/Path-Finding-Algorithm-Visualizer",
+  },
+];
+
+
+export const skills = [
+  "TypeScript",
+  "React",
+  "Next.js",
+  "C++",
+  "Neural Networks",
+  "AWS-Stepfunction",
+];
